4f88697caddea87dc9021edf1f49adb488b669a1
akma <tazhiyeva.akmaral@bk.ru>
2022-10-19 23:04:09 +0600

    123

--- Assignment-1/Landmarks/Model/ModelData.swift
+++ Assignment-1/Landmarks/Model/ModelData.swift
@@ -2,11 +2,9 @@ import Foundation
 // import Combine
 
 final class ModelData: ObservableObject {
-    // An observable object needs to publish any changes to its data
-    // so that its subscribers can pick up the change
+   
     @Published var landmarks: [Landmark] = load("landmarkData.json")
-    // No need to mark it with @Published attribute
-    // since we'll never modify hike data after initially loading it
+    
     var hikes: [Hike] = load("hikeData.json")
     @Published var profile = Profile.default
 
